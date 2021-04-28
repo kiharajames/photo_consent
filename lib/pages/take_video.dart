97786fb1839100db4e4a395d51f9a1b38de3f2af
kiharajames <ikiharajames@gmail.com>
2021-04-28 14:20:51 +0300

    Added play video functionality after recording...

--- lib/pages/take_video.dart
+++ lib/pages/take_video.dart
@@ -3,6 +3,7 @@ import 'package:camera/camera.dart';
 import 'dart:async';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:video_player/video_player.dart';
 
 import 'package:photo_consent/public_variables.dart';
 import 'package:photo_consent/style.dart';
@@ -18,6 +19,9 @@ class _TakeVideoState extends State<TakeVideo> {
   bool checkValue = false;
   CameraController _controller;
   Future<void> _initializeControllerFuture;
+  VideoPlayerController _controllervideoplay;
+  Future<void> _initializeVideoPlayerFuture;
+  bool videoIsRecording = false;
 
   @override
   void initState() {
@@ -43,92 +47,169 @@ class _TakeVideoState extends State<TakeVideo> {
     super.dispose();
   }
 
+  void initVideoState(String file) {
+    // Create an store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.]
+    setState(() {
+      _controllervideoplay = VideoPlayerController.file(file);
+      _initializeVideoPlayerFuture = _controllervideoplay.initialize();
+    });
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Take consent video'),
       ),
-      body: Container(
-        color: Colors.grey,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Text('Please state the following:', style: TextStyle(fontSize: 17),),
-              Card(
-                color: Colors.blue[200],
-                child: Container(
-                  margin: EdgeInsets.all(10),
-                    child: Text('"I consent to physical activities up to and including intercourse with my partner, state name of partner"', style: TextStyle(
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.grey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Card(
+                  color: Colors.blue[200],
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                      child: Text('Please state the following: "I consent to physical activities up to and including intercourse with my partner, state name of partner"', style: TextStyle(
 
-                    ),)),
+                      ),)),
 
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
+                ),
+                Card(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
 
-                  SizedBox(
-                    height: 230,
-                    child: Card(
-                      elevation: 10,
-                      child: FutureBuilder<void>(
-                        future: _initializeControllerFuture,
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState == ConnectionState.done) {
-                            // If the Future is complete, display the preview.
-                            return CameraPreview(_controller);
-                          } else {
-                            // Otherwise, display a loading indicator.
-                            return Center(child: CircularProgressIndicator());
+                      SizedBox(
+                        height: 230,
+                        child: Card(
+                          elevation: 10,
+                          child: FutureBuilder<void>(
+                            future: _initializeControllerFuture,
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState == ConnectionState.done) {
+                                // If the Future is complete, display the preview.
+                                return CameraPreview(_controller);
+                              } else {
+                                // Otherwise, display a loading indicator.
+                                return Center(child: CircularProgressIndicator());
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                      FloatingActionButton(
+                          child: Icon(Icons.crop_square),
+                          backgroundColor: Colors.redAccent,
+                          onPressed: () async {
+                            //get storage path
+
+                            final Directory appDirectory = await getApplicationDocumentsDirectory();
+                            final String videoDirectory = '${appDirectory.path}/Videos';
+                            await Directory(videoDirectory).create(recursive: true);
+                            final String currentTime = DateTime.now().millisecondsSinceEpoch.toString();
+                            final String filePath = '$videoDirectory/${currentTime}.mp4';
+
+                            if (!_controller.value.isRecordingVideo) {
+                              _controller.startVideoRecording();
+                              setState(() {
+                                videoIsRecording = true;
+                              });
+                            }
+                            if (_controller.value.isRecordingVideo) {
+                              XFile videoFile = await _controller.stopVideoRecording();
+                              print(videoFile.path);//and there is more in this XFile object
+                              initVideoState(videoFile.path);
+                              setState(() {
+                                videoIsRecording = false;
+                              });
+                            }
                           }
-                        },
+                      ),
+                      videoIsRecording == true ? Text('Stop recording') : Text('Start Recording')
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                      flex: 2,
+                      child: Checkbox(value: checkValue,
+                          onChanged: (bool value){
+                        setState(() {
+                          checkValue = value;
+                        });
+                          }
                       ),
                     ),
-                  ),
-                  FloatingActionButton(
-                      child: Icon(Icons.crop_square),
-                      backgroundColor: Colors.redAccent,
-                      onPressed: () async {
-                        //get storage path
+                    Flexible(
+                      flex: 4,
+                      child: Text('I prefer my partner perform practices deemed as "safe sex practice" or risk losing my consent', style: TextStyle(fontWeight: FontWeight.bold),),)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
 
-                        final Directory appDirectory = await getApplicationDocumentsDirectory();
-                        final String videoDirectory = '${appDirectory.path}/Videos';
-                        await Directory(videoDirectory).create(recursive: true);
-                        final String currentTime = DateTime.now().millisecondsSinceEpoch.toString();
-                        final String filePath = '$videoDirectory/${currentTime}.mp4';
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 200,
+                          child: Card(
+                            child: _initializeVideoPlayerFuture != null ? FutureBuilder(
+                              future: _initializeVideoPlayerFuture,
+                              builder: (context, snapshot) {
+                                if (snapshot.connectionState == ConnectionState.done) {
+                                  // If the VideoPlayerController has finished initialization, use
+                                  // the data it provides to limit the aspect ratio of the VideoPlayer.
+                                  return AspectRatio(
+                                    aspectRatio: _controllervideoplay.value.aspectRatio,
+                                    // Use the VideoPlayer widget to display the video.
+                                    child: VideoPlayer(_controllervideoplay),
+                                  );
+                                } else {
+                                  // If the VideoPlayerController is still initializing, show a
+                                  // loading spinner.
+                                  return Center(child: CircularProgressIndicator());
+                                }
+                              },
+                            ) : SizedBox(height: 0,),
+                          ),
+                        ),
+                        _initializeVideoPlayerFuture != null ? FloatingActionButton(
+                          onPressed: () {
+                            // Wrap the play or pause in a call to `setState`. This ensures the
+                            // correct icon is shown
+                            setState(() {
+                              // If the video is playing, pause it.
+                              if (_controllervideoplay.value.isPlaying) {
+                                _controllervideoplay.pause();
+                              } else {
+                                // If the video is paused, play it.
+                                _controllervideoplay.play();
+                              }
+                            });
+                          },
+                          // Display the correct icon depending on the state of the player.
+                          child: Icon(
+                            _controllervideoplay.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                          ),
+                        ) : SizedBox(height: 0,)
 
-                        if (!_controller.value.isRecordingVideo) {
-                          _controller.startVideoRecording();
-                        }
-                        if (_controller.value.isRecordingVideo) {
-                          XFile videoFile = await _controller.stopVideoRecording();
-                          print(videoFile.path);//and there is more in this XFile object
-                        }
-                      }
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Flexible(
-                    flex: 2,
-                    child: Checkbox(value: checkValue,
-                        onChanged: (bool value){
-                      setState(() {
-                        checkValue = value;
-                      });
-                        }
+                      ],
                     ),
-                  ),
-                  Flexible(
-                    flex: 4,
-                    child: Text('I prefer my partner perform practices deemed as "safe sex practice" or risk losing my consent', style: TextStyle(fontWeight: FontWeight.bold),),)
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
 
+            ),
           ),
         ),
       ),
